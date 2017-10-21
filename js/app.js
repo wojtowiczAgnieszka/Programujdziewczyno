97bf1f267c4099b4506564a4fa34a0e061849b9f
Agnieszka <arkadia1990@wp.pl>
2017-10-21 16:19:54 +0200

    zwijalne menu

--- js/app.js
+++ js/app.js
@@ -10,6 +10,23 @@ document.addEventListener('DOMContentLoaded', function() {
     
     menuElement.addEventListener('mouseout', function(){
         dropdownList.style.display = 'none';
-        
-    });
+        });
+    
+    //zadanie2
+    
+    var buttons = document.querySelectorAll('.btn.read-more');
+   
+     function showHide() {
+       var textArea = this.previousElementSibling;
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+          textArea.style.display = 'block';
+         this.innerHTML ='mniej<span class="glyphicon glyphicon-chevron-up"></span>';
+      } else{
+          textArea.style.display= 'none';
+          this.innerHTML = 'więcej<span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+     }
+    for (var i=0; i < buttons.length; i++){
+         buttons[i].addEventListener('click', showHide);
+    }
 });
